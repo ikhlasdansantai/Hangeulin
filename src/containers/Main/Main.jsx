132667368d6eb6eb129af8ac8ba5d5195b2dc03f
ikhlasdansantai <initesting5@gamil.com>
2023-06-26 23:13:00 +0700

    menampung seluruh comp ke comp parent (main)

---
+++ src/containers/Main/Main.jsx
@@ -0,0 +1,13 @@
+import Price from "../../components/Price/Price";
+import Services from "../../components/Services/Services";
+
+const Main = () => {
+  return (
+    <main>
+      <Services />
+      <Price />
+    </main>
+  );
+};
+
+export default Main;
