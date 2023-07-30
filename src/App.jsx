2d33d8b7a36eb1c7299ecb2e137acfab8cec5d14
ikhlasdansantai <initesting5@gamil.com>
2023-07-30 22:53:48 +0700

    menghapus padding, comp yang gak butuh padding jadi bisa atur sendiri padding nya

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
+import Footer from "./containers/Footer/Footer";
 import Header from "./containers/Header/Header";
 import Main from "./containers/Main/Main";
 
 function App() {
   return (
     <>
-      <div id="containers" className="min-[1400px]:w-[90rem] mx-auto p-standart h-[2000000000px]">
+      <div id="containers" className="min-[1400px]:w-[90rem] mx-auto  h-[2000000000px]">
         <Header />
         <Main />
+        <Footer />
       </div>
     </>
   );
