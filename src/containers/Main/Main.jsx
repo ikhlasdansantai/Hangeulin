e5fa888a857d830aecff4f192547860955218150
ikhlasdansantai <initesting5@gamil.com>
2023-07-31 11:52:21 +0700

    menambahkan comp joinwithus pada comp main

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,6 +1,7 @@
 import Price from "../../components/section/Price/Price";
 import Services from "../../components/section/Services/Services";
 import Testimonials from "../../components/section/Testimonials/Testimonials";
+import JoinWithUs from "../../components/section/joinWithUs/JoinWithUs";
 
 const Main = () => {
   return (
@@ -8,6 +9,7 @@ const Main = () => {
       <Services />
       <Price />
       <Testimonials />
+      <JoinWithUs />
     </main>
   );
 };
