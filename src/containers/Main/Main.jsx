1b68bc8cc0928e5f89b7e53f41bcbd9eaaae8ff2
ikhlasdansantai <initesting5@gamil.com>
2023-07-31 11:27:24 +0700

    menambahkan testimonials pada comp main

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,11 +1,13 @@
 import Price from "../../components/section/Price/Price";
 import Services from "../../components/section/Services/Services";
+import Testimonials from "../../components/section/Testimonials/Testimonials";
 
 const Main = () => {
   return (
     <main className="p-standart">
       <Services />
       <Price />
+      <Testimonials />
     </main>
   );
 };
